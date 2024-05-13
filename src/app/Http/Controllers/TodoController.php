f98ad5821b4bd13038d8fbd6d7598ed8548ff1e6
Makoto.S <makosawai0208@gmail.com>
2024-05-13 19:18:12 +0900

    update: TodoControllerをリファクタリングし、可読性と保守性を向上

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -7,40 +7,37 @@ use App\Todo;
 
 class TodoController extends Controller
 {
-    //
+    private $todo;
+
+    public function __construct(Todo $todo)
+    {
+        $this->todo = $todo;
+    }
+
     public function index()
     {
-        $todo = new Todo();
-        $todoList = $todo->all();
-       
+        $todoList = $this->todo->all();
         return view('todo.index', ['todoList' => $todoList]);
     }
     public function create()
     {
-    // TODO: 第1引数を指定
-    return view('todo.create'); // 追記
+        return view('todo.create');
     }
 
-    public function store(Request $request) // 追記
+    public function store(Request $request)
     {
-    $inputs = $request->all(); // 変更
+        $inputs = $request->all();
 
-    $content = $request->input('content'); // 追記
-     // 1. todosテーブルの1レコードを表すTodoクラスをインスタンス化
-     $todo = new Todo();
-     // 2. Todoインスタンスのカラム名のプロパティに保存したい値を代入
-     $todo->fill($inputs); // 変更
-     // 3. Todoインスタンスの`->save()`を実行してオブジェクトの状態をDBに保存するINSERT文を実行
-     $todo->save();
+        $content = $request->input('content');
+        $this->todo->fill($inputs);
+        $this->todo->save();
 
-     return redirect()->route('todo.index'); // 追記
+        return redirect()->route('todo.index');
     }
 
     public function show($id)
     {
-        $model = new Todo();
-        $todo = $model->find($id);
-       
+        $todo = $this->todo->find($id);
         return view('todo.show', ['todo' => $todo]);
     }
 }
